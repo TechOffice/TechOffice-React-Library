7f468fe7da81b25bdcfe4376014a8d0e06383490
ben <ben@techoffice.com>
2019-07-17 22:55:38 +0800

    update

---
+++ 140-Finalform/010-HelloWorldExample/src/Form.tsx
@@ -0,0 +1,11 @@
+
+import * as React from "react";
+import { createForm } from 'react-final-form'
+
+@(createForm({
+    onSubmit
+}) as any)
+export default class extends React.Component<any, any> {
+
+    
+}
