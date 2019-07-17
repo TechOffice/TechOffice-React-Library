a3570bd9f108969190de79e477b75aee09716845
ben <ben@techoffice.com>
2019-07-17 23:05:18 +0800

    update

--- 140-Finalform/010-HelloWorldExample/src/App.tsx
+++ 140-Finalform/010-HelloWorldExample/src/App.tsx
@@ -1,12 +1,27 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Form, Field } from 'react-final-form'
 
 class App extends React.Component{
     
+    onSubmit(values){
+        alert("submitted" + JSON.stringify(values));
+    }
+
     render(){
         return (
             <div>
                 <h1>Hello World</h1>
+                <Form onSubmit={this.onSubmit}
+                    render={({ handleSubmit, pristine, invalid }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Field name="firstName" component="input" placeholder="First Name" />
+                            <button type="submit" disabled={pristine || invalid}>
+                                Submit
+                            </button>
+                        </form>
+                    )}>
+                </Form>
             </div>            
         )
     }
